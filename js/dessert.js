996c772485cf2a797d11cc74086e1424897fe6fb
Dan Stocker <dan@kwaia.com>
2012-12-29 15:48:30 +0100

    function assertions and adding custom type

--- js/dessert.js
+++ js/dessert.js
@@ -29,6 +29,40 @@ var dessert;
 
         isStringStrict: function (expr) {
             return this.assert(expr instanceof String);
+        },
+
+        isFunction: function (expr) {
+            return this.assert(typeof expr === 'function');
+        },
+
+        isFunctionOptional: function (expr) {
+            return this.assert(
+                typeof expr === 'undefined' ||
+                typeof expr === 'function'
+            );
+        },
+
+        /**
+         * Adds a new assertion method.
+         * @param methodName {string} Name of new method
+         * @param validator {function} Function validating a given type.
+         * In it, `this` will refer to the `dessert` namespace.
+         */
+        addType: function (methodName, validator) {
+            dessert
+                .isString(methodName)
+                .isFunction(validator);
+
+            if (!dessert.hasOwnProperty(methodName)) {
+                dessert[methodName] = function () {
+                    validator.apply(dessert, arguments);
+                    return dessert; // making sure method returns namespace
+                };
+            } else {
+                throw Error("Assertion type already taken.");
+            }
+
+            return dessert;
         }
     };
 }());
--- js/dessert.test.js
+++ js/dessert.test.js
@@ -1,4 +1,4 @@
-/*global dessert, module, test, raises, equal */
+/*global dessert, module, test, raises, equal, ok */
 (function () {
     module('dessert');
 
@@ -19,6 +19,60 @@
 
         raises(function () {
             dessert.isString(1);
-        }, "Numeric fails string assertion");
+        }, "Numeric (non-string) fails string assertion");
+    });
+
+    test("Function assertion", function () {
+        equal(dessert.isFunction(function () {}), dessert, "Function passes assertion");
+
+        raises(function () {
+            dessert.isFunction();
+        }, "Undefined fails string assertion");
+
+        raises(function () {
+            dessert.isFunction("hello");
+        }, "String (non-function) fails string assertion");
+    });
+
+    test("Optional function assertion", function () {
+        equal(dessert.isFunctionOptional(function () {}), dessert, "Function passes assertion");
+
+        equal(dessert.isFunctionOptional(), dessert, "Undefined passes assertion");
+
+        raises(function () {
+            dessert.isFunctionOptional('foo');
+        }, "String (non-function) fails string assertion");
+    });
+
+    test("Type addition", function () {
+        ok(!dessert.hasOwnProperty('test'), "New type is not pre-existing (sanity check)");
+
+        raises(function () {
+            dessert.addType(1, function () {});
+        }, "Invalid methodName passed");
+
+        raises(function () {
+            dessert.addType('test', 'foo');
+        }, "Invalid validator passed (non-function)");
+
+        dessert.addType('test', function (expr) {
+            // this will assert on expr not being equal to 'test'
+            this.assert(expr === 'test');
+        });
+
+        ok(dessert.hasOwnProperty('test'), "New property added to namespace");
+
+        raises(function () {
+            dessert.addType('test', function () {});
+        }, "Attempting to overwrite custom validator");
+
+        equal(dessert.test('test'), dessert, "Custom assertion passed");
+
+        raises(function () {
+            dessert.test('foo');
+        }, "Custom assertion failed");
+
+        // cleanup
+        delete dessert.test;
     });
 }());
