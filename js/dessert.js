b691deee7f8b99df7b824f205452c1fada830914
Dan Stocker <dan@kwaia.com>
2012-12-30 03:27:14 +0100

    soft mode

--- js/dessert.js
+++ js/dessert.js
@@ -16,80 +16,31 @@ var dessert;
             }
         },
 
-        isString: function (expr) {
-            return this.assert(typeof expr === 'string');
-        },
-
-        isStringOptional: function (expr) {
-            return this.assert(
-                typeof expr === 'undefined' ||
-                typeof expr === 'string'
-            );
-        },
-
-        isStringStrict: function (expr) {
-            return this.assert(expr instanceof String);
-        },
-
-        isFunction: function (expr) {
-            return this.assert(typeof expr === 'function');
-        },
-
-        isFunctionOptional: function (expr) {
-            return this.assert(
-                typeof expr === 'undefined' ||
-                typeof expr === 'function'
-            );
-        },
-
-        isObject: function (expr) {
-            return this.assert(expr instanceof Object);
-        },
-
-        isObjectOptional: function (expr) {
-            return this.assert(
-                typeof expr === 'undefined' ||
-                expr instanceof Object
-            );
-        },
-
-        isPlainObject: function (expr) {
-            return this.assert(
-                expr instanceof Object &&
-                Object.getPrototypeOf(expr) === Object.prototype
-            );
-        },
-
-        isArray: function (expr) {
-            return this.assert(expr instanceof Array);
-        },
-
-        isArrayOptional: function (expr) {
-            return this.assert(
-                typeof expr === 'undefined' ||
-                expr instanceof Array
-            );
-        },
-
         /**
-         * Adds a new assertion method.
+         * Adds a new validator.
          * @param methodName {string} Name of new method
          * @param validator {function} Function validating a given type.
          * In it, `this` will refer to the `dessert` namespace.
          * Expected to return boolean.
          */
         addType: function (methodName, validator) {
-            dessert
-                .isString(methodName)
-                .isFunction(validator);
-
-            if (!dessert.hasOwnProperty(methodName)) {
-                dessert[methodName] = function () {
-                    dessert.assert(validator.apply(dessert, arguments));
-                    return dessert; // making sure method returns namespace
-                };
-            } else {
-                throw Error("Custom assertion name ('" + methodName + "') already taken.");
+            if (typeof methodName === 'string' &&
+                typeof validator === 'function'
+                ) {
+                if (!dessert.hasOwnProperty(methodName)) {
+                    dessert[methodName] = function () {
+                        var success = validator.apply(dessert, arguments);
+                        if (arguments[1] === true) {
+                            // soft mode: skipping actual assertion
+                            return success;
+                        } else {
+                            dessert.assert(success);
+                            return dessert; // making sure method returns namespace
+                        }
+                    };
+                } else {
+                    throw Error("Custom assertion name ('" + methodName + "') already taken.");
+                }
             }
 
             return dessert;
@@ -120,4 +71,64 @@ var dessert;
             return dessert;
         }
     };
+
+    /**
+     * Adding basic validators
+     */
+    dessert.addTypes({
+        isString: function (expr) {
+            return typeof expr === 'string';
+        },
+
+        isStringOptional: function (expr) {
+            return (
+                typeof expr === 'undefined' ||
+                typeof expr === 'string'
+                );
+        },
+
+        isStringStrict: function (expr) {
+            return expr instanceof String;
+        },
+
+        isFunction: function (expr) {
+            return typeof expr === 'function';
+        },
+
+        isFunctionOptional: function (expr) {
+            return (
+                typeof expr === 'undefined' ||
+                typeof expr === 'function'
+                );
+        },
+
+        isObject: function (expr) {
+            return expr instanceof Object;
+        },
+
+        isObjectOptional: function (expr) {
+            return (
+                typeof expr === 'undefined' ||
+                expr instanceof Object
+                );
+        },
+
+        isPlainObject: function (expr) {
+            return (
+                expr instanceof Object &&
+                Object.getPrototypeOf(expr) === Object.prototype
+                );
+        },
+
+        isArray: function (expr) {
+            return expr instanceof Array;
+        },
+
+        isArrayOptional: function (expr) {
+            return (
+                typeof expr === 'undefined' ||
+                expr instanceof Array
+                );
+        }
+    });
 }());
--- js/dessert.test.js
+++ js/dessert.test.js
@@ -10,62 +10,14 @@
         }, "Failed assertion raises exception");
     });
 
-    test("String assertion", function () {
-        equal(dessert.isString("hello"), dessert, "String passes assertion");
-
-        raises(function () {
-            dessert.isString();
-        }, "Undefined fails string assertion");
-
-        raises(function () {
-            dessert.isString(1);
-        }, "Numeric (non-string) fails string assertion");
-    });
-
-    test("Function assertion", function () {
-        equal(dessert.isFunction(function () {}), dessert, "Function passes assertion");
-
-        raises(function () {
-            dessert.isFunction();
-        }, "Undefined fails string assertion");
-
-        raises(function () {
-            dessert.isFunction("hello");
-        }, "String (non-function) fails string assertion");
-    });
-
-    test("Optional function assertion", function () {
-        equal(dessert.isFunctionOptional(function () {}), dessert, "Function passes assertion");
-
-        equal(dessert.isFunctionOptional(), dessert, "Undefined passes assertion");
-
-        raises(function () {
-            dessert.isFunctionOptional('foo');
-        }, "String (non-function) fails string assertion");
-    });
-
-    test("Plain object assertion", function () {
-        equal(dessert.isPlainObject({}), dessert, "Plain object passes assertion");
-
-        raises(function () {
-            dessert.isPlainObject(Object.prototype);
-        }, "`Object.prototype` fails assertion");
-
-        raises(function () {
-            dessert.isPlainObject(Object.create({}));
-        }, "Derived object fails assertion");
-    });
-
     test("Type addition", function () {
         ok(!dessert.hasOwnProperty('test'), "New type is not pre-existing (sanity check)");
 
-        raises(function () {
-            dessert.addType(1, function () {});
-        }, "Invalid methodName passed");
+        dessert.addType(1, function () {});
+        ok(!dessert.hasOwnProperty(1), "Invalid method name passed (namespace was not changed)");
 
-        raises(function () {
-            dessert.addType('test', 'foo');
-        }, "Invalid validator passed (non-function)");
+        dessert.addType('test', 'foo');
+        ok(!dessert.hasOwnProperty('test'), "Invalid validator passed (non-function, namespace was not changed)");
 
         dessert.addType('test', function (expr) {
             // returning a boolean expression to be passed to `.assert`
@@ -106,5 +58,56 @@
 
         // cleanup
         delete dessert.test;
+    });
+
+    test("String assertion", function () {
+        equal(dessert.isString("hello"), dessert, "String passes assertion");
+
+        raises(function () {
+            dessert.isString();
+        }, "Undefined fails string assertion");
+
+        raises(function () {
+            dessert.isString(1);
+        }, "Numeric (non-string) fails string assertion");
+    });
+
+    test("Soft mode", function () {
+        equal(dessert.isString("hello", true), true, "Soft mode success");
+        equal(dessert.isString(null, true), false, "Soft mode failure (null instead of string)");
+    });
+
+    test("Function assertion", function () {
+        equal(dessert.isFunction(function () {}), dessert, "Function passes assertion");
+
+        raises(function () {
+            dessert.isFunction();
+        }, "Undefined fails string assertion");
+
+        raises(function () {
+            dessert.isFunction("hello");
+        }, "String (non-function) fails string assertion");
+    });
+
+    test("Optional function assertion", function () {
+        equal(dessert.isFunctionOptional(function () {}), dessert, "Function passes assertion");
+
+        equal(dessert.isFunctionOptional(), dessert, "Undefined passes assertion");
+
+        raises(function () {
+            dessert.isFunctionOptional('foo');
+        }, "String (non-function) fails string assertion");
+    });
+
+    test("Plain object assertion", function () {
+        equal(dessert.isPlainObject({}), dessert, "Plain object passes assertion");
+
+        raises(function () {
+            dessert.isPlainObject(Object.prototype);
+        }, "`Object.prototype` fails assertion");
+
+        raises(function () {
+            dessert.isPlainObject(Object.create({}));
+        }, "Derived object fails assertion");
     });
 }());
