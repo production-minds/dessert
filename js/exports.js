4e92881b775fdfbfeeaea14dd5ab1914c5571564
Dan Stocker <dan@kwaia.com>
2013-06-16 12:16:05 +0200

    namespace exported directly

--- js/exports.js
+++ js/exports.js
@@ -1,7 +1,7 @@
 /**
  * Library exports
  */
-/*global dessert, exports */
-if (typeof exports === 'object') {
-    exports.dessert = dessert;
+/*global dessert, module */
+if (typeof module === 'object') {
+    module.exports = dessert;
 }
